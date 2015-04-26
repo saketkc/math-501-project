946c98f9adc624d4f27041c5cdb9cc40b2e237cb
Nachikethas <nachi@server.fake>
2015-04-26 16:16:36 -0700

    Possible fixes for A

--- src/main.jl
+++ src/main.jl
@@ -1,10 +1,13 @@
 ## Constants
-tau = 10/400.0
-h = 1/20.0
+m = 21          # The number of 'space' nodes
+n = 401         # The number of 'time' nodes
+tau = 10/(n-1)  # Time step size
+h = 1/(m-1)     # Space step size
+
 lamb = 0.3
 taubyh = tau/h
-x = linrange(0,1,20)
-t = linrange(0,10,400)
+x = linrange(0,1,m-1)
+t = linrange(0,10,n-1)
 
 function mu(y)
     return 1/(1-y)^2
@@ -15,12 +18,10 @@ function muTau(y)
 end
 
 function calculateA()
-    A = zeros(20,20)
-    for i=2:20
-        A[i-1,i-1] = 1 - muTau(x[i-1]) + taubyh
-        if i<20
-            A[i-1,i+1] = 1 - muTau(x[i-1]) -taubyh
-        end
+    A = zeros(m,m)
+    for i=2:m-1
+        A[i,i-1] = 1 - muTau(x[i]) + taubyh
+        A[i,i+1] = 1 - muTau(x[i]) - taubyh
     end
    return A
 end
