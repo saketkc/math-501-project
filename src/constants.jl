12b921de8cd4ba008b567baa48fe9e22ae0fb56f
Saket Choudhary <saketkc@gmail.com>
2015-04-26 17:42:29 -0700

    alpha constant

--- src/constants.jl
+++ src/constants.jl
@@ -5,6 +5,10 @@ h = 1/(m-1)     # Space step size
 
 λ = 0.3
 τbyh = τ/h
-α = 0.3+0.1*(1-cos(1))         # Integral of g(x) = 0.3 +0.1sin(x) = 0.3+0.1(1-cos1)
 x = linrange(0,1,m)
 t = linrange(0,10,n)
+
+
+α = 0.3+0.1*(1-cos(1))         # Integral of g(x) = 0.3 +0.1sin(x) = 0.3+0.1(1-cos1)
+β = #TODO
+γ = (1-λ*τ+h*τ/2)
