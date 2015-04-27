5b455bff9ea64e9b8dce1408e63a42b5ba95fa53
Nachikethas <nachi@server.fake>
2015-04-26 18:00:25 -0700

    Added the definition of optimal p0

--- src/constants.jl
+++ src/constants.jl
@@ -4,9 +4,13 @@ n = 401         # The number of 'time' nodes
 h = 1/(m-1)     # Space step size
 
 λ = 0.3
+π = 3.14159265359
 α = 0.3+0.1*(1-cos(1))         # Integral of g(x) = 0.3 +0.1sin(x) = 0.3+0.1(1-cos1)
 x = linrange(0,1,m)'
 t = linrange(0,10,n)'
-u = μ(x); u[1] = 0; u = u';
+u = μ(x); u[1] = 0; u = u';    # Vector form of mu
 β = #TODO
 γ = (1-λ*τ+h*τ/2)
+
+optv = 0.85 + 0.05cos(2π*t)   # The optimal distribution we are aiming at
+
