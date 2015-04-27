26a03881d6403f878ee6d348757f5440cfeee9e4
Nachikethas <nachi@server.fake>
2015-04-26 17:14:15 -0700

    progagating constants.jl mods

--- src/functions.jl
+++ src/functions.jl
@@ -1,16 +1,12 @@
-function mu(y)
+function μ(y)
     return 1/(1-y)^2
-end
-
-function muTau(y)
-    return tau*mu(y)
 end
 
 function calculateA()
     A = zeros(m,m)
     for i=2:m-1
-        A[i,i-1] = 1 - muTau(x[i]) + taubyh
-        A[i,i+1] = 1 - muTau(x[i]) - taubyh
+        A[i,i-1] = 1 - τ*μ(x[i]) + τ/h
+        A[i,i+1] = 1 - τ*μ(x[i]) - τ/h
     end
    return A
 end
